c35eb5b23e080c94d8188eb3da397c32ed65c4d2
alex <alex.toledo.zavala.10@gmail.com>
2024-04-11 20:32:37 -0500

    primer commit

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+/* seleccionar una etiqueta del HTML */
+const selector = document.getElementById("mensaje");
+
+/* escribir dentro de esa etiqueta con JS */
+selector.innerHTML = "CONEXIÓN EXITOSA ENTRE HTML Y JS";
+
+const nombre = prompt("Ingresa tu nombre: ");   /*  MUESTRA UNA VENTANA */
+
+console.log("Nombre: " + nombre);
+
+alert("Bienvenido " + nombre);
+
+
+const frase = "Buenas Noches" ;
+
+fraseMin = frase.toLowerCase();
+fraseMay = frase.toUpperCase();
+
+console.log(fraseMin);
+console.log(fraseMay);
